ce57c251c22ddd38abdffe47c2dd1bdfb65b5ec4
QuinnCaris <50358541+Quinnca77@users.noreply.github.com>
2023-10-05 15:21:55 +0200

    Added some comments

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{fs, cmp::Ordering};
 
 const R_EARTH: f64 = 6371000.0;
 
+// Wrapper struct for determining Ordering on f64 primitive type
 #[derive(PartialEq,PartialOrd)]
 struct NonNan(f64);
 
@@ -30,9 +31,11 @@ fn main() {
     println!("{}", find_street_name(50.816486, 6.005317, addr_vec));
 }
 
+#[allow(unused_assignments)]
 fn find_addr_vec(contents: String) -> Vec<(f64, f64, String)> {
     let contents_lines = contents.lines().collect::<Vec<&str>>();
     let mut addr_vec: Vec<(f64, f64, String)> = Vec::new();
+    // Add every address in the .osm file to addr_vec
     for (line_index, line) in contents_lines.iter().enumerate() {
         if line.contains("addr:city") {
                 let lat_index = contents_lines[line_index - 1].find("lat=").unwrap_or(200) + 4;
@@ -40,11 +43,14 @@ fn find_addr_vec(contents: String) -> Vec<(f64, f64, String)> {
                 if lat_index > 200 {
                     continue;
                 }
+
                 let lat_index_end = contents_lines[line_index - 1][lat_index..contents_lines[line_index - 1].len()].chars().skip(1).position(|x| x == '"').unwrap() + 1 + lat_index;
                 let lon_index_end = contents_lines[line_index - 1][lon_index..contents_lines[line_index - 1].len()].chars().skip(1).position(|x| x == '"').unwrap() + 1 + lon_index;
                 let lat = contents_lines[line_index - 1][(lat_index + 1)..lat_index_end].parse::<f64>().unwrap();
                 let lon = contents_lines[line_index - 1][(lon_index + 1)..lon_index_end].parse::<f64>().unwrap();
                 let mut street_name = String::new();
+
+                // There shouldn't be any other lines on which the street is described, but if there is, then it should be easy to patch
                 if contents_lines[line_index + 3].contains("addr:street") {
                     let street_name_index = contents_lines[line_index + 3].find("v=").unwrap() + 3;
                     street_name = contents_lines[line_index + 3][street_name_index..(contents_lines[line_index + 3].len() - 3)].to_string();
@@ -71,12 +77,14 @@ fn find_street_name(lat: f64, lon: f64, addr_vec: Vec<(f64, f64, String)>) -> St
         let delta_phi = (lat_addr-lat).to_radians();
         let delta_lambda = (lon_addr-lon).to_radians();
 
-        //formula haversine distance: https://www.movable-type.co.uk/scripts/latlong.html or https://rust-lang-nursery.github.io/rust-cookbook/science/mathematics/trigonometry.html
+        // Formula haversine distance: https://www.movable-type.co.uk/scripts/latlong.html or https://rust-lang-nursery.github.io/rust-cookbook/science/mathematics/trigonometry.html
         let a = (delta_phi / 2.0).sin().powi(2) + (phi1.cos() * phi2.cos() * (delta_lambda / 2.0).sin().powi(2));
         let c = 2.0 * a.sqrt().asin();
         let distance = R_EARTH * c;
         distance_vec.push((distance, street_name));
     }
+    // To get the minimal distance, we use the wrapper struct introduced earlier
     let non_nan_vec: Vec<(NonNan, String)> = distance_vec.iter().map(|x| (NonNan::new(x.0).unwrap(), x.1.clone())).collect();
+    // We find the minimal value in the wrapper struct (which will be the smallest distance to the closest building) and extract the street_name of this element
     return non_nan_vec.iter().min_by(|x, y| x.0.cmp(&y.0)).unwrap().1.clone();
 }
