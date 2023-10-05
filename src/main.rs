80124debf004bd84772927ac513e9c9e862159ea
QuinnCaris <50358541+Quinnca77@users.noreply.github.com>
2023-10-05 12:58:15 +0200

    Completed the street_name function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
-use std::fs;
+use std::{fs, cmp::Ordering};
+
+const R_EARTH: f64 = 6371000.0;
+
+#[derive(PartialEq,PartialOrd)]
+struct NonNan(f64);
+
+impl NonNan {
+    fn new(val: f64) -> Option<NonNan> {
+        if val.is_nan() {
+            None
+        } else {
+            Some(NonNan(val))
+        }
+    }
+}
+
+impl Eq for NonNan {}
+
+impl Ord for NonNan {
+    fn cmp(&self, other: &NonNan) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
 
 
 fn main() {
     let contents = fs::read_to_string("map.osm").unwrap();
-    // let string = "schnitzel\"schnitzel";
-    // let index = string.chars().position(|x| x == '"').unwrap();
-    //println!("{:?}", find_addr(contents));
-    println!("{:?}", find_addr_vec(contents));
+    let addr_vec = find_addr_vec(contents);
+    println!("{}", find_street_name(50.816486, 6.005317, addr_vec));
 }
 
 fn find_addr_vec(contents: String) -> Vec<(f64, f64, String)> {
@@ -34,10 +55,8 @@ fn find_addr_vec(contents: String) -> Vec<(f64, f64, String)> {
                     let street_name_index = contents_lines[line_index + 4].find("v=").unwrap() + 3;
                     street_name = contents_lines[line_index + 4][street_name_index..(contents_lines[line_index + 4].len() - 3)].to_string();
                 } else {
-                    println!("{}", contents_lines[line_index + 2]);
-                    panic!();
+                    panic!("street_name could not be found in the usual lines");
                 };
-                //let street_name_index = contents_lines[line_index + 2].find("v=").unwrap() + 3;
                 addr_vec.push((lat, lon, street_name));
         }
     }
@@ -45,6 +64,19 @@ fn find_addr_vec(contents: String) -> Vec<(f64, f64, String)> {
 }
 
 fn find_street_name(lat: f64, lon: f64, addr_vec: Vec<(f64, f64, String)>) -> String {
-    //use haversine distance
-    return String::from("");
+    let mut distance_vec = Vec::new();
+    let phi1 = lat.to_radians();
+    for (lat_addr, lon_addr, street_name) in addr_vec {
+        let phi2 = lat_addr.to_radians();
+        let delta_phi = (lat_addr-lat).to_radians();
+        let delta_lambda = (lon_addr-lon).to_radians();
+
+        //formula haversine distance: https://www.movable-type.co.uk/scripts/latlong.html or https://rust-lang-nursery.github.io/rust-cookbook/science/mathematics/trigonometry.html
+        let a = (delta_phi / 2.0).sin().powi(2) + (phi1.cos() * phi2.cos() * (delta_lambda / 2.0).sin().powi(2));
+        let c = 2.0 * a.sqrt().asin();
+        let distance = R_EARTH * c;
+        distance_vec.push((distance, street_name));
+    }
+    let non_nan_vec: Vec<(NonNan, String)> = distance_vec.iter().map(|x| (NonNan::new(x.0).unwrap(), x.1.clone())).collect();
+    return non_nan_vec.iter().min_by(|x, y| x.0.cmp(&y.0)).unwrap().1.clone();
 }
